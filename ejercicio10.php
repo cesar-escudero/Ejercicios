096ea974a3fedae7a4040499f041d563aa6038b7
cesar-escudero <30834367+cesar-escudero@users.noreply.github.com>
2017-08-09 07:47:24 -0500

    Add files via upload

---
+++ ejercicio10.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <title>Ejercicio 10 </title>
+  </head>
+  <body>
+
+
+<h4>Numeros Del 1 Al 10 valores Correctos</h4>
+
+
+        <?php
+      $c=1;
+      $e=10;
+
+        while ($c <= $e) {
+          echo $c;
+
+          $c++;
+
+        }
+
+         ?>
+
+       </form>
+  </body>
+</html>
